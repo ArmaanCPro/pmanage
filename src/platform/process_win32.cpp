42f9f8ebd988acd05978902574056bca937e3fd8
Armaan Chahal <armaan.c2009@gmail.com>
2025-12-18 16:54:48 -0600

    feat: add windows process support and implementation

--- src/platform/process_win32.cpp
+++ src/platform/process_win32.cpp
@@ -1,5 +1,90 @@
+module;
+
+#if defined(_WIN32)
+
+#include <windows.h>
+#include <TlHelp32.h>
+#include <tchar.h>
+
+#else
+#pragma warning "Compiling process_win32.cpp not allowed on non-windows platforms is not allowed!"
+#endif
+
+#include <expected>
+#include <memory>
+#include <string>
+
 module pmanage;
 
-#ifdef _WIN32
+namespace pmng
+{
 
-#endif
+struct win32_process : public process
+{
+    win32_process(DWORD pid) : m_Pid(pid)
+    {
+    }
+
+    [[nodiscard]] virtual std::expected<void, std::string> kill() noexcept override
+    {
+        HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, m_Pid);
+
+        if (!hProcess)
+        {
+            return std::unexpected{ "Could not open process." };
+        }
+
+        BOOL result = TerminateProcess(hProcess, 0);
+
+        CloseHandle(hProcess);
+
+        if (!result)
+        {
+            return std::unexpected{ "Could not terminate process." };
+        }
+
+        return {};
+    }
+
+    [[nodiscard]] virtual pid_t pid() const noexcept override { return static_cast<pid_t>(m_Pid); }
+
+private:
+   DWORD m_Pid;
+};
+
+[[nodiscard]] std::expected<std::unique_ptr<process>, std::string> process::find_by_name(const std::string &name) noexcept
+{
+    DWORD pid = 0;
+    // takes a snapshot of all processes
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (hSnapshot != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 pe;
+        pe.dwSize = sizeof(PROCESSENTRY32);
+
+        if (Process32First(hSnapshot, &pe))
+        {
+            do
+            {
+                if (_tcsicmp(pe.szExeFile, name.c_str()) == 0)
+                {
+                    pid = pe.th32ProcessID;
+                    break;
+                }
+            }
+            while (Process32Next(hSnapshot, &pe));
+        }
+
+        CloseHandle(hSnapshot);
+    }
+
+    return std::make_unique<win32_process>(pid);
+}
+
+[[nodiscard]] std::expected<std::unique_ptr<process>, std::string> process::find_by_pid(pid_t pid) noexcept
+{
+    return std::make_unique<win32_process>(static_cast<DWORD>(pid));
+}
+
+} // namespace pmng
